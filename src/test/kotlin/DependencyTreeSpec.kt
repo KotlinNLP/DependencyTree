c20cb3eaa130598d081d0f4f0dcb003c24f64356
KotlinNLP Authors <github@kotlinnlp.com>
2017-12-07 14:07:35 +0100

    Fix projectiveOrder and add inOrder test

--- src/test/kotlin/DependencyTreeSpec.kt
+++ src/test/kotlin/DependencyTreeSpec.kt
@@ -124,10 +124,17 @@ class DependencyTreeSpec : Spek({
         ArcConfiguration(11, 8)
       ))
 
+      on("inOrder") {
+
+        it("should return the expected list") {
+          assertEquals(listOf(0, 1, 2, 3, 7, 8, 9, 10, 11, 4, 5, 6), dependencyTree.inOrder())
+        }
+      }
+
       on("projectiveOrder") {
 
         it("should return the expected list") {
-          assertEquals(listOf(0, 1, 2, 3, 7, 8, 9, 10, 11, 4, 5, 6), dependencyTree.projectiveOrder())
+          assertEquals(listOf(0, 1, 2, 3, 9, 10, 11, 4, 5, 6, 7, 8), dependencyTree.projectiveOrder())
         }
       }
 
